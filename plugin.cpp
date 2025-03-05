cb4c1963552c679336e879dff1613f611ca2d5a7
Mrowr Purr <mrowr.purr@gmail.com>
2025-03-04 19:49:59 -0800

    SKSE example

--- plugin.cpp
+++ plugin.cpp
@@ -1,17 +1,1 @@
-#include <MyStaticLibrary.h>
-#include <RE/Skyrim.h>
-#include <SKSE/SKSE.h>
-
-extern "C" __declspec(dllexport) bool SKSEPlugin_Load(const SKSE::LoadInterface* a_skse) {
-    SKSE::Init(a_skse);
-
-    SKSE::GetMessagingInterface()->RegisterListener(
-        "SKSE",
-        [](SKSE::MessagingInterface::Message* a_msg) {
-            if (a_msg->type == SKSE::MessagingInterface::kDataLoaded)
-                MyStaticLibrary().PrintSomethingInConsole();
-        }
-    );
-
-    return true;
-}
+#include <SkyrimScripting/Plugin.h>
